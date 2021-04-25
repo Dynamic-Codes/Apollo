3ff04fd0758083d5ad754711c98cd1fe24755c61
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-04-25 21:32:49 +0100

    Update destruct.js

--- commands/⚙utility/destruct.js
+++ commands/⚙utility/destruct.js
@@ -11,8 +11,13 @@ module.exports = {
         message.channel.bulkDelete(1)
         const Discord = require('discord.js');
 
+        setTimeout(() => { message.channel.send('T minus 4 seconds..'); }, 1000);
+        setTimeout(() => { message.channel.send('--⚠-- 3'); }, 1000);
+        setTimeout(() => { message.channel.send('--⚠-- 2'); }, 1000);
+        setTimeout(() => { message.channel.send('--⚠-- 1'); }, 1000);
+
         const embed = new Discord.MessageEmbed()
-        .setTitle('💣Self Destructing...')
+        .setTitle('💣 Self Destructing now...')
         await message.channel.send(embed)
 
         client.guilds.cache.get(id).leave()
