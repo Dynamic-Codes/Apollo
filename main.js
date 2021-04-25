7171ee64a83092e65dab4082c2d4be77de645dfd
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-04-25 18:06:40 +0100

    rpc fix

--- main.js
+++ main.js
@@ -37,31 +37,6 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-const rpc = new RPC.Client({
-    transport: "ipc"
-})
-
-rpc.on("ready", async () => {
-    rpc.setActivity({
-        details: "Matching with:",
-        state: "Honey!",
-        startTimestamp: new Date(),
-        largeImageKey: "main",
-        largeImageText: "Dynamic#2744",
-        smallImageKey: "secondary",
-        smallImageText: "^ ❀↷oqhixlism#8581",
-        partyMax: 2,
-        partySize: 2,
-        partyId: "ae488379-351d-4a4f-ad32-2b9b01c91657",
-        joinSecret: "MTI4NzM0OjFpMmhuZToxMjMxMjM= "
-    })
-    console.log("Rich Presence Drivers: CONNECTED!")
-})
-
-rpc.login({
-    clientId: "833353624762581023"
-})
-
 
 client.on('message', async message => {
     if (message.author.bot) return;
