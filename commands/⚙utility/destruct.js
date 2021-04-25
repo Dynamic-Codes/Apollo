9bcbc4e2497804edbd352ffe7a5674386d4a0863
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-04-25 21:18:45 +0100

    Create destruct.js

---
+++ commands/⚙utility/destruct.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'leave',
+    description: 'Leave a server', 
+    args: true,
+    usage: '<Guild ID>',
+    guildOnly: true,
+    ownerOnly: true,
+    aliases: ['destruct', 'self-destruct'],
+    execute(message, args, client) {
+        const id = args[0]
+        message.channel.bulkDelete(1)
+        const Discord = require('discord.js');
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle('💣Self Destructing...')
+        message.channel.send(embed)
+
+        client.guilds.cache.get(id).leave()
+        .catch(err => {
+            console.log(`there was an error leaving the guild: \n ${err.message}`);
+        })
+    }
+};
