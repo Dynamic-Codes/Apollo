9df38e70f4912f2483445940b7ffa85ea4ce2ac5
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-04-25 18:10:32 +0100

    rpc fix

--- main.js
+++ main.js
@@ -21,6 +21,32 @@ const commandFolders = fs.readdirSync('./commands');
 //     require(`./handler/${handler}`)(client, Discord);
 // })
 
+const RPC = require("discord-rpc")
+const rpc = new RPC.Client({
+    transport: "ipc"
+})
+
+rpc.on("ready", async () => {
+    rpc.setActivity({
+        details: "Matching with:",
+        state: "Honey!",
+        startTimestamp: new Date(),
+        largeImageKey: "main",
+        largeImageText: "Dynamic#2744",
+        smallImageKey: "secondary",
+        smallImageText: "^ ❀↷oqhixlism#8581",
+        partyMax: 2,
+        partySize: 2,
+        partyId: "ae488379-351d-4a4f-ad32-2b9b01c91657",
+        joinSecret: "MTI4NzM0OjFpMmhuZToxMjMxMjM= "
+    })
+    console.log("Rich Presence Drivers: CONNECTED!")
+})
+
+rpc.login({
+    clientId: "833353624762581023"
+})
+
 
 for (const folder of commandFolders) {
 	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
