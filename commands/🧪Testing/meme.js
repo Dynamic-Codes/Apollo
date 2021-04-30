9d4ab86c8136f6576a464662adbaf6ffbcd69f04
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-04-30 20:05:54 +0100

    Update meme.js

--- commands/🧪Testing/meme.js
+++ commands/🧪Testing/meme.js
@@ -3,25 +3,27 @@ module.exports = {
     description: 'A meme command just like Dank memer!',
     guildOnly: true,
     async execute(message, args, client) {
-        const subReddits = ["dankmeme", "meme", "me_irl"];
+        const Discord = require('discord.js')
+        const fetch = require("node-fetch");
+        const subReddits = ["memes"];
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-        try {
-            const { body } = await snekfetch
-                .get('https://www.reddit.com/r/${random}.json?sort=top&t=week')
-                .query({ limit: 800 });
-            const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-            if (!allowed.length) return message.channel.send('It seems we are out of memes');
-            const randomnumber = Math.floor(Math.random() * allowed.length)
-            const embed = new Discord.RichEmbed()
-                .setColor(0x00A2E8)
-                .setTitle(allowed[randomnumber].data.title)
-                .setDescription("Posted by: " + allowed[randomnumber].data.author)
-                .setImage(allowed[randomnumber].data.url)
-                .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-                .setFooter("r/" + random)
-            message.channel.send(embed)
-        } catch (err) {
-            return console.log(err);
-        }
+        const url = `https://www.imgur.com/r/${random}/hot.json`;
+        const res = await fetch(url);
+        const json = await res.json();
+        const posts = json.data
+            .filter(f => msg.channel.nsfw || !f.nsfw);
+
+        if (!posts.length) return message.channel.send("No posts, maybe try in a nsfw channel");
+
+        const post = posts[Math.floor(Math.random() * posts.length)];
+        const redditUrl = `https://www.reddit.com${post.reddit}`;
+        const embed = new Discord.MessageEmbed()
+            .setColor('PURPLE')
+            .setImage(`https://imgur.com/${imageData.hash}${imageData.ext.replace(/\?.*/, '')}`)
+            .setTitle("Post from r/" + random)
+            .setURL(redditUrl)
+            //this might be imgur upvotes or reddit upvotes idk
+            .addField("Votes", post.score)
+        message.channel.send(embed);
     }
 };
