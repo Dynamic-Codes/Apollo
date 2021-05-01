e5e5cd25db55f7db06ee9bd3f9a25871fa33dada
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-01 16:31:33 +0100

    Create lock.js

---
+++ commands/🔒moderation/lock.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: 'lock',
+    description: 'Lock and Unlock channels!',
+    args: true,
+    usage: '<on/off>',
+    guildOnly: true,
+    aliases: ['ex', 'eg'],
+    execute(message, args, client) {
+        const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        if (args[0] === 'on') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: false
+                }).then(() => {
+                    channel.setName(channel.name += `🔒`)
+                })
+            })
+            return message.channel.send('locked all channels');
+        } else if (args[0] === 'off') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: true
+                }).then(() => {
+                        channel.setName(channel.name.replace('🔒', ''))
+                    }
+                )
+            })
+            return message.channel.send('unlocked all channels')
+        }
+    }
+};
+
