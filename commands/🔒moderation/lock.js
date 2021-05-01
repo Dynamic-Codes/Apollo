541146c5d6f0d682f25dad7122403b367f751647
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-01 16:44:27 +0100

    Update lock.js

--- commands/🔒moderation/lock.js
+++ commands/🔒moderation/lock.js
@@ -6,13 +6,23 @@ module.exports = {
     guildOnly: true,
     aliases: ['ex', 'eg'],
     execute(message, args, client) {
+        const Discord = require('discord.js')
         const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+
+        const LockEmb = new Discord.MessageEmbed()
+        .setTitle('🔒 LOCKDOWN ACTIVE')
+
+        const UnlockEmb = new Discord.MessageEmbed()
+        .setTitle('🔓 LOCKDOWN DISABLED')
+
+
         if (args[0] === 'on') {
             channels.forEach(channel => {
                 channel.updateOverwrite(message.guild.roles.everyone, {
                     SEND_MESSAGES: false
                 }).then(() => {
                     channel.setName(channel.name += `🔒`)
+                    channel.send(LockEmb)
                 })
             })
             return message.channel.send('locked all channels');
@@ -22,6 +32,7 @@ module.exports = {
                     SEND_MESSAGES: true
                 }).then(() => {
                         channel.setName(channel.name.replace('🔒', ''))
+                        channel.send(UnlockEmb)
                     }
                 )
             })
