cd965984dc532c48327051092d5df33056251c61
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-01 12:48:50 +0100

    Create anime.js

---
+++ commands/🧪Testing/anime.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'anime',
+    description: 'Memes but Anime Style!',
+    guildOnly: true,
+    async execute(message, args, client) {
+        const Discord = require('discord.js');
+        const got = require('got');
+        const embed = new Discord.MessageEmbed()
+        got('https://www.reddit.com/r/Animemes/random/.json').then(response => {
+            let content = JSON.parse(response.body);
+            let permalink = content[0].data.children[0].data.permalink;
+            let memeUrl = `https://reddit.com${permalink}`;
+            let memeImage = content[0].data.children[0].data.url;
+            let memeTitle = content[0].data.children[0].data.title;
+            let memeUpvotes = content[0].data.children[0].data.ups;
+            let memeDownvotes = content[0].data.children[0].datsa.downs;
+            let memeNumComments = content[0].data.children[0].data.num_comments;
+            embed.setTitle(`${memeTitle}`)
+            embed.setURL(`${memeUrl}`)
+            embed.setImage(memeImage)
+            embed.setColor('RANDOM')
+            embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+            message.channel.send(embed);
+        })
+    }
+};
