014eefa9531e4b8288c991cf9d63db3cb1d3e3f6
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-03 18:03:18 +0100

    Update roleinfo.js

--- commands/🧪Testing/roleinfo.js
+++ commands/🧪Testing/roleinfo.js
@@ -31,41 +31,5 @@ module.exports = {
             .setFooter('Apollo Project')
 
         message.channel.send(embed)
-
-        try {
-            const roleName = message.guild.roles.cache.find(r => (r.name === args.toString()) || (r.id === args.toString()))
-            console.log(roleName)
-            const perms = new permissions(roleName.permissions.bitfield).toArray()
-
-            const embed = new MessageEmbed()
-                .setColor(roleName.color)
-                .setTitle(roleName.name)
-                .addFields(
-                    {
-                        name: 'Role ID: ',
-                        value: roleName.id,
-                        inline: true
-                    },
-                    {
-                        name: 'Role Name: ',
-                        value: roleName.name,
-                        inline: true
-                    },
-                    {
-                        name: 'Mentionable: ',
-                        value: roleName.mentionable ? 'Yes' : 'No',
-                        inline: true
-                    },
-                    {
-                        name: 'Role Permissions: ',
-                        value: perms.join(', ')
-                    }
-                )
-
-            await message.channel.send(embed)
-
-        } catch (e) {
-            return message.channel.send('Role Doesn\'t Exist').then(() => console.log(e))
-        }
     }
 };
