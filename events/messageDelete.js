55ce384906f22b946b46953ebee9a7fff2f29718
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-08 16:57:13 +0100

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,18 +1,12 @@
 module.exports = {
 	name: 'messageDelete',
 	execute(message, client) {
-		const LOG = GuildAuditLog.entries.first()
-        if (!LOG) return;
-
-        const { executor } = LOG;
-
         message.channel.send(
             new MessageEmbed()
                 .setColour('RED')
                 .SetAuthor('Message Delete', executor.avatarURL())
                 .setDescription(
-                    `**Deleted by:** ${executor.username}#${executor.discriminator}
-                    **Author:** ${message.author.username}#${message.author.discriminator}
+                    `**Author:** ${message.author.username}#${message.author.discriminator}
                     **Channel:** ${message.channel}
                     **Content:** \`\`\`${message.content}\`\`\``
                 )
