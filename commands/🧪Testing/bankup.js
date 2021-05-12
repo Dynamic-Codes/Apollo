789713f5cc38f35761c8e1e2e04e194dbb5d5d7a
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-12 18:54:26 +0100

    Update bankup.js

--- commands/🧪Testing/bankup.js
+++ commands/🧪Testing/bankup.js
@@ -47,7 +47,7 @@ module.exports = {
                 .setFooter('🚀 ApolloProject')
 
             await message.channel.send(BankBed)
-            return message.channel.send(`Use \`${guildProfile.prefix}bankup <ID>\` to make upragde!`)
+            return message.channel.send(`Use \`${guildProfile.prefix}bankup <ID>\` to make upgrade!`)
         }
 
         if (!["hv-ephs", "hv-titv", "hv-trtn", "hv-sdna", "hv-huma", "hv-kplr"].includes(bankType).toLowerCase()) return message.channel.send('꒰🤔꒱ ꒦ Did you provide an invalid deal ID? ꒷')
