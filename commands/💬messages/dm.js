4bbe186a4191d940a58822e595e821f419d384f9
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-16 11:41:51 +0100

    Update dm.js

--- commands/💬messages/dm.js
+++ commands/💬messages/dm.js
@@ -11,8 +11,8 @@ module.exports = {
         if(message.author.id !== '614829609665560687') UImsg = (`${message.author.username} | ${message.guild.name}\n`)
         if(mentiondm == null) return message.reply('Beep Boing: No user to send message to!');
         mentionMessage = args.slice(1).join(' ');
-        finalmsg = (`${UImsg} ${mentionMessage}`)
-        mentiondm.send(mentionMessage);            
+        const finalmsg = (`${UImsg} ${mentionMessage}`)
+        mentiondm.send(finalmsg);            
         console.log('Message Sent!')
     }
 };
