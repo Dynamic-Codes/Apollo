c40aef51a15de729ff28cef5609920a38a6876ce
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-29 16:55:18 +0100

    fixed button handler

--- events/clickButton.js
+++ events/clickButton.js
@@ -1,11 +1,8 @@
 module.exports = {
 	name: 'clickButton',
 	async execute(button, client) {
-        require('discord-buttons')(client)
-
         if (button.id === 'ping') {
-            button.defer()
-            button.channel.send('Pong!')
+            button.channel.send('Pong!');
         }
 	},
 };
