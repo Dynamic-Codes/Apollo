ac445ce69e15cc67ab84a778e15085843855724e
Dynamic Coding <55919825+Dynamic-Codes@users.noreply.github.com>
2021-05-30 18:09:47 +0100

    add member join event

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,29 @@
+module.exports = {
+	name: 'guildMemberAdd',
+	async execute(guildMember, client) {
+        const Guild = require('../models/guildSchema')
+        let guildProfile = await Guild.findOne({
+            guildID: message.guild.id
+        });
+
+        let chx = guildProfile.auditLogID;
+
+        let role = guildProfile.joinRoleID;
+
+        if(!guildProfile.auditLogID) return;
+
+        let welcomeRole = guildMember.guild.roles.cache.get(role);
+ 
+        guildMember.roles.add(welcomeRole);
+
+        const Discord = require('discord.js')
+        client.channels.cache.get(chx).send(
+            new Discord.MessageEmbed()
+                .setDescription(
+                    `**User:** ${guildMember.username}#${guildMember.discriminator}`
+                )
+                .setColor('GREEN')
+                .setFooter('Member Join', guildMember.displayAvatarURL({ dynamic: true }))
+        )
+	},
+};
